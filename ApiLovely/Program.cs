181f1c6ab377fb3ad099bfd0960117948e8af4c7
luanamayane <luana.mayane@escolar.ifrn.edu.br>
2023-12-26 09:23:25 -0300

    Adicionado o Identity no program.cs

--- ApiLovely/Program.cs
+++ ApiLovely/Program.cs
@@ -1,4 +1,5 @@
 using ApiLovely.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,9 @@ builder.Services.AddDbContext<ApiLovelyContext>(options => options.UseNpgsql(con
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApiLovelyContext>()
+    .AddDefaultTokenProviders();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
