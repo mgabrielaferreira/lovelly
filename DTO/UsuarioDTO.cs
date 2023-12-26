0a56332d2912deb7bed385ec5391ee3fc1661b3d
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-12-26 09:39:41 -0300

    corrigindo erro ortográfico em usuario dto

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -9,6 +9,6 @@ namespace ApiUniversidade.DTO
     {
       public string Email { get; set;}
       public string Password { get; set;}
-      public string ConfirmPassword {get; set;} 
+      public string ConfirmPassword { get; set;} 
     }
 }
