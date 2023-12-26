afefdd3f7dcd256b38005b63b21cd965fe1cff4e
JoyceMBrito <joyce.brito@escolar.ifrn.edu.br>
2023-12-26 10:33:27 -0300

    adicionando metodo pos registre e login

--- ApiLovely/Controllers/AutorizaController.cs
+++ ApiLovely/Controllers/AutorizaController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using ApiLovely.Migrations;
+using ApiUniversidade.DTO;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +27,37 @@ namespace ApiLovely.Controllers
     public ActionResult<string> Get(){
         return "AutorizaController :: Acessado em : "
             + DateTime.Now.ToLongDateString();
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody]UsuarioDTO model){
+        var user = new IdentityUser{
+            UserName = model.Email,
+            Email = model.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if(!result.Succeeded)
+            return BadRequest(result.Errors);
+        
+        await _signInManager.SignInAsync(user, false);
+        //return Ok(GerarToken(model));
+        return Ok();
+
+    
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody]UsuarioDTO userInfo){
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password,
+            isPersistent: false,lockoutOnFailure : false);
+
+            if(result.Succeeded)
+               return Ok();
+            else{
+                ModelState.AddModelError(string.Empty, "Login Inválido...");
+                return BadRequest(ModelState);
+            }
     }
     }
     
